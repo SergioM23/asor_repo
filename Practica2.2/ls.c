5d2217c5cf206fbcdb1eb37b14669fcc48d4cdc0
SergioM23 <75066252+SergioM23@users.noreply.github.com>
2020-12-16 09:46:41 +0100

    Add files via upload

---
+++ Practica2.2/ls.c
@@ -0,0 +1,69 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//open
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+
+
+	if (argc < 2) {
+		 printf("ERROR: Especifica el nombre del directorio.\n");
+		 return -1;
+	}
+
+  DIR *directorio = opendir(argv[1]);
+
+  if (directorio == NULL ) {
+    printf("ERROR: No existe el directorio.\n");
+    return -1;
+  }
+
+  struct dirent *current;
+  struct stat info;
+  size_t sizePath = strlen(argv[1]);
+  current = readdir(directorio);
+  unsigned long int totalsize = 0;
+
+  while (current != NULL) {
+    char *path = malloc(sizeof(char)*(sizePath + strlen(current->d_name) + 3));
+    strcpy(path, argv[1]);
+    strcat(path, "/");
+    strcat(path, current->d_name);
+
+
+    if (stat(path, &info) == -1) {
+      printf("ERROR : No se puede analizar el archivo\n");
+      free(path);
+      closedir(directorio);
+      return -1;
+    } else {
+      if (S_ISREG(info.st_mode)) {
+        printf("[*] %s \n", current->d_name);
+        totalsize = totalsize + ((info.st_blksize/8)*info.st_blocks);
+
+      } else if (S_ISDIR(info.st_mode)) {
+        printf("[/] %s \n", current->d_name);
+
+      } else if (S_ISLNK(info.st_mode)) {
+        char *linkname = malloc(info.st_size + 1);
+        int rc2 = readlink(path, linkname, info.st_size + 1);
+        printf("[S]: %s->%s \n", current->d_name, linkname);
+        free(linkname);
+	     }
+    }
+
+    free(path);
+    current = readdir(directorio);
+  }
+  printf("Tamaño total: %d bytes\n", totalsize);
+  printf("Tamaño total: %d KB\n", totalsize / 1024);
+  closedir(directorio);
+  return 0;
+}
